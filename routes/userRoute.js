87162bfbca428c454b63a7d0d661e080deec4312
fahmyfauzi <fahmyfauzii@gmail.com>
2023-10-22 21:50:29 +0700

    unfollow user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -26,13 +26,21 @@ router.get("/friends/:userId", async (req, res) => {
 
 //follow user
 router.put("/:id/follow", async (req, res) => {
-  console.log(req.body._id, req.params.id);
+  // Memeriksa apakah pengguna yang melakukan permintaan tidak sama dengan pengguna yang diikuti
   if (req.body.userId != req.params.id) {
     try {
+      // Mencari pengguna yang diikuti berdasarkan ID yang diberikan
       const user = await User.findById(req.params.id);
+
+      // Mencari pengguna yang melakukan permintaan berdasarkan ID yang diberikan
       const currentUser = await User.findById(req.body.userId);
+
+      // Memeriksa apakah pengguna yang melakukan permintaan belum mengikuti pengguna yang diikuti
       if (!user.followers.includes(req.body.userId)) {
+        // Mengikuti pengguna yang diikuti dengan menambahkan pengikut
         await user.updateOne({ $push: { followers: req.body.userId } });
+
+        // Memperbarui pengguna yang melakukan permintaan dengan menambahkan yang diikuti
         await currentUser.updateOne({ $push: { followings: req.params.id } });
         return res.status(200).json("User has been followed");
       } else {
@@ -46,4 +54,31 @@ router.put("/:id/follow", async (req, res) => {
   }
 });
 
+//unfollow user
+router.put("/:id/unfollow", async (req, res) => {
+  // Pengecekan apakah pengguna yang melakukan permintaan tidak sama dengan pengguna yang akan di-unfollow
+  if (req.body.userId !== req.params.id) {
+    try {
+      // Mencari pengguna yang akan di-unfollow berdasarkan ID yang diberikan
+      const user = await User.findById(req.params.id);
+
+      // Mencari pengguna yang melakukan permintaan berdasarkan ID yang diberikan
+      const currentUser = await User.findById(req.body.userId);
+
+      // Memeriksa apakah pengguna yang melakukan permintaan sudah mengikuti pengguna yang akan di-unfollow
+      if (!user.followers.includes(req.body.userId)) {
+        // Melakukan unfollow dengan menghapus pengikut (followers)
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+
+        // Menghapus pengguna yang melakukan permintaan dari daftar followers pengguna yang akan di-unfollow
+        await currentUser.updateOne({ $pull: { followers: req.params.id } });
+        return res.status(200).json("User has been unfollowed");
+      } else {
+        return res.status(403).json("You dont follow this user");
+      }
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  }
+});
 module.exports = router;
